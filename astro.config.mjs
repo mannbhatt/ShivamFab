fd3bd355fb337981aced1327de0549c8b93947ea
Mann <Mann@88.com>
2024-12-27 11:44:57 +0530

    add vercel astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
     tailwind() // Add Tailwind CSS integration
   ],
 output: 'static',
+adapter: vercel(),
   server: {
     host: true, // Allow access from the local network
   },
